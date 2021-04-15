cd9341c3d0a918f9ef6607a9d40a796e3653d331
Carlo Harvey <carlo.harvey@bcu.ac.uk>
2021-04-15 21:51:22 +0100

    Initial Commit for Ray Tracing Starter Code
    Implements Lab from week 9 and wraps in an SDL visualisation

---
+++ raytracer.cpp
@@ -0,0 +1,112 @@
+// A practical implementation of the ray tracing algorithm.
+
+#include "geometry.h"
+#include "SDL.h" 
+#include <fstream>
+#include <chrono>
+
+#define M_PI 3.14159265359
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Surface* screen;
+
+void init() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Software Rasteriser",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    screen = SDL_GetWindowSurface(window);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+}
+
+void putpixel(SDL_Surface* surface, int x, int y, Uint32 pixel)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to set */
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp) {
+    case 1:
+        *p = pixel;
+        break;
+
+    case 2:
+        *(Uint16*)p = pixel;
+        break;
+
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        }
+        else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32*)p = pixel;
+        break;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // initialise SDL2
+    init();
+
+    SDL_Event e;
+    bool running = true;
+    while (running) {
+
+        auto t_start = std::chrono::high_resolution_clock::now();
+
+        // clear back buffer, pixel data on surface and depth buffer (as movement)
+        SDL_FillRect(screen, nullptr, SDL_MapRGB(screen->format, 0, 0, 0));
+        SDL_RenderClear(renderer);
+        
+        auto t_end = std::chrono::high_resolution_clock::now();
+        auto passedTime = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+        std::cerr << "Frame render time:  " << passedTime << " ms" << std::endl;
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, screen);
+        if (texture == NULL) {
+            fprintf(stderr, "CreateTextureFromSurface failed: %s\n", SDL_GetError());
+            exit(1);
+        }
+        SDL_FreeSurface(screen);
+
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+        SDL_DestroyTexture(texture);
+
+        if (SDL_PollEvent(&e))
+        {
+            switch (e.type) {
+            case SDL_KEYDOWN:
+                switch (e.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    running = false;
+                    break;
+                }
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
